278d1cec13091168a2c9abd5e6131f8b1d2e4798
Christopher Voigt <chlorophyllkid@gmail.com>
2019-05-12 12:42:00 +0200

    add react  and react-dom

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
 const commonjs = require('rollup-plugin-commonjs');
 const { uglify } = require('rollup-plugin-uglify');
-const { minify } = require('uglify-es');
+const { minify } = require('terser');
 
 module.exports = {
   input: 'src/_es6/bundle.js',
   plugins: [
     resolve(),
-    commonjs(),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    commonjs({
+      namedExports: {
+        'node_modules/react/index.js': ['Component', 'PropTypes'],
+        'node_modules/react-dom/index.js': ['render'],
+      },
+    }),
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
-      presets: [['@babel/preset-env', { modules: false }]],
+      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
     }),
     uglify({}, minify),
   ],
