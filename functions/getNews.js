539db30321f5d50be9d5a535a651998b3840e322
Christopher <chlorophyllkid@gmail.com>
2020-05-24 21:30:11 +0200

    move functions from import to require

--- functions/getNews.js
+++ functions/getNews.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 const api_key = '96eb5c7142df4063b326c651073940ab';
 const api = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${api_key}`;
