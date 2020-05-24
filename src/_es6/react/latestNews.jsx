4816663204be763ba344340699273dee81fd51e1
Christopher <chlorophyllkid@gmail.com>
2020-05-24 21:41:58 +0200

    fix empty response in rendering

--- src/_es6/react/latestNews.jsx
+++ src/_es6/react/latestNews.jsx
@@ -14,34 +14,35 @@ export default function latestNews() {
 }
 
 
-class NewsList extends React.Component {
+const NewsList = ({ articles }) => {
+  const news = [];
+  const articles = this.props.articles;
 
-  render() {
-    const news = [];
-    const articles = this.props.articles;
+  articles.forEach((article) => {
+    const date = new Date(article.publishedAt)
+    const dateString= date.toLocaleDateString('de-DE', { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit'});
+    const timeSting = date.toLocaleTimeString('de-DE', { formatMatcher: 'best fit' });
 
-    articles.forEach((article) => {
-      const date = new Date(article.publishedAt)
-      const dateString= date.toLocaleDateString('de-DE', { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit'});
-      const timeSting = date.toLocaleTimeString('de-DE', { formatMatcher: 'best fit' });
+    let image = '';
+    if (article.urlToImage) {
+      image = <img src={article.urlToImage} className='latestNews_articleImage'/>
+    }
 
-      let image = '';
-      if (article.urlToImage) {
-        image = <img src={article.urlToImage} className='latestNews_articleImage'/>
-      }
+    news.push(
+      <article className='latestNews_article'>
+        <span className='latestNews_articleDate' aria-label='veröffentlicht am'>{`${dateString} - ${timeSting} Uhr`}</span>
+        <h2 className='latestNews_articleTitle'>
+          <a href={article.url} title={article.title}>{article.title}</a>
+        </h2>
+        {image}
+        <p className='latestNews_articleDescription'>{article.description}</p>
+      </article>
+    );
+  });
 
-      news.push(
-        <article className='latestNews_article'>
-          <span className='latestNews_articleDate' aria-label='veröffentlicht am'>{`${dateString} - ${timeSting} Uhr`}</span>
-          <h2 className='latestNews_articleTitle'>
-            <a href={article.url} title={article.title}>{article.title}</a>
-          </h2>
-          {image}
-          <p className='latestNews_articleDescription'>{article.description}</p>
-        </article>
-      );
-    });
+  return news;
+}
 
-    return news;
-  }
+NewsList.defaultProps = {
+  articles: [],
 }
