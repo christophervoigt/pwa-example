dcf5e27d0ef1ed7b83314a8ff37119cd8b7473fa
Christopher <chlorophyllkid@gmail.com>
2020-05-24 21:58:45 +0200

    check for rewrite in commonjs

--- functions/getNews.js
+++ functions/getNews.js
@@ -3,12 +3,12 @@ const fetch = require("node-fetch");
 const api_key = '96eb5c7142df4063b326c651073940ab';
 const api = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${api_key}`;
 
-exports.handler = async (event, context) => {
+module.exports = function handler(event, context) {
   return fetch(api, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
+    .then(function (response) { return response.json() })
+    .then(function (data) { return ({
       statusCode: 200,
       body: data
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+    })})
+    .catch(function (error) { return ({ statusCode: 422, body: String(error) }) });
 };
