1e03a56e1ae120df6dedb3970e180018a97a0760
Christopher <chlorophyllkid@gmail.com>
2020-05-24 22:09:28 +0200

    clean up function

--- functions/getNews.js
+++ functions/getNews.js
@@ -1,16 +1,10 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-const api_key = '96eb5c7142df4063b326c651073940ab';
-const api = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${api_key}`;
+exports.handler = (event, context, callback) => {
+  const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
-exports.handler = function (event, context, callback) {
-  return fetch(api, { headers: { "Accept": "application/json" } })
-    .then(function (response) { return response.json() })
-    .then(function (data) {
-      return callback(null, {
-        statusCode: 200,
-        body: data,
-      })
-    })
-    .catch(function (error) { return ({ statusCode: 422, body: String(error) }) });
-};
+  return fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => callback(null, { statusCode: 200, body: data }))
+    .catch((error) => callback({ statusCode: 422, body: error }));
+}
