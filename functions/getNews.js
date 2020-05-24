2e699f866c4804fbf8cac1840e596642fb5237f3
Christopher <chlorophyllkid@gmail.com>
2020-05-24 22:16:49 +0200

    remove callback and exclude api endpoint

--- functions/getNews.js
+++ functions/getNews.js
@@ -1,10 +1,10 @@
 import fetch from "node-fetch";
 
-exports.handler = (event, context, callback) => {
-  const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
+const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
+exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT)
     .then((response) => response.json())
-    .then((data) => callback(null, { statusCode: 200, body: data }))
-    .catch((error) => callback({ statusCode: 422, body: error }));
+    .then((data) => ({ statusCode: 200, body: data }))
+    .catch((error) => ({ statusCode: 422, body: error }));
 }
