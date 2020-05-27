263cc19aa22fa7085231ee1546e220e848c3f25c
Christopher <chlorophyllkid@gmail.com>
2020-05-27 23:08:25 +0200

    fix getNews function

--- functions/getNews.js
+++ functions/getNews.js
@@ -1,10 +1,17 @@
-import fetch from "node-fetch";
+const axios = require('axios');
 
 const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT)
-    .then((response) => response.json())
-    .then((data) => ({ statusCode: 200, body: data }))
-    .catch((error) => ({ statusCode: 422, body: error }));
-}
+exports.handler = async (event, context, callback) => {
+  return axios.get(API_ENDPOINT)
+    .then((response) => {
+      callback(null, {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(response.data),
+      });
+    })
+    .catch((error) => {
+      callback(error);
+    });
+};
